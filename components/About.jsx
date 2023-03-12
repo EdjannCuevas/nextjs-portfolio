d9a7ded143d066a9f07168b17d8ae1aa7597db9f
Ed Cuevas <edjanncuevas@icloud.com>
2023-03-12 21:15:45 +0900

    feat: add about me description

--- components/About.jsx
+++ components/About.jsx
@@ -7,7 +7,10 @@ const About = () => {
             <div className='col-span-2'>
                 <p>About</p>
                 <h2>Who I am</h2>
-                <p>placeholder placeholderplaceholderplaceholder placeholder placeholder placeholder placeholder placeholder placeholderplaceholder placeholder placeholder placeholderplaceholderplaceholderplaceholder placeholderplaceholderplaceholder </p>
+                <p>Hello! My name is <span className='text-[#46a8fd]'>Edgardo Cuevas</span> , and I'm a software engineer based in Kanagawa. I'm passionate about building high-quality software that makes people's lives easier and more enjoyable.</p><br />
+                <p>When I'm not coding, you can usually find me indulging in my other passions. I'm a huge movie buff and love nothing more than settling in for a marathon session of my favorite films. I also like to stay active by working out and going for walks while listening to podcasts.</p><br/>
+                <p>One of the things I love about software engineering is that it's a constantly evolving field. There's always something new to learn, whether it's a new programming language, a new tool or framework, or a new approach to problem-solving. I'm always up for a challenge and love pushing myself to grow and improve as a developer. </p><br />
+                <p>If you're looking for a software engineer who is passionate about their work and brings a unique blend of technical skills and creative thinking to the table, I'd love to hear from you. Let's work together to build something amazing!</p>
             </div>
             <div></div>
         </div>
