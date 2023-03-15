ccd9bb0339ca6144747b93874e1c0f0213955e4f
Ed Cuevas <edjanncuevas@icloud.com>
2023-03-15 18:40:02 +0900

    style: intial stype for projexts page

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div id='projects' className='w-full md:h-screen p-2 flex items-center py-16'>
-        <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
-            <div>Projects</div>
+    <div
+      id='projects'
+      className='w-full md:h-screen p-2 flex py-20'
+    >
+        <div className='max-w-[1240px] flex'>
+            <div>
+              <p className='text-xl tracking-widest uppercase text-[#46a8fd]'>Projects</p>
+              <h2 className='py-4'>What I Can Do</h2>
+            </div>
         </div>
     </div>
   )
