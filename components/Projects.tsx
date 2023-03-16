719548d3c4a7e512224e93cf0f6e08051818e3e2
Ed Cuevas <edjanncuevas@icloud.com>
2023-03-16 12:41:43 +0900

    style: change width and lenth for mobile

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,18 +14,22 @@ import Tailwind from '../public/images/skills/tailwind.png'
 import Postgres from '../public/images/skills/postgres.png'
 import Typescript from '../public/images/skills/typescript.png'
 
-const Projects = () => {
+interface ProjectsProps {
+  handleLink: (arg: string) => void;
+}
+
+const Projects: React.FC<ProjectsProps> = ({ handleLink }) => {
 
   return (
     <div
       id='projects'
-      className='w-full md:h-screen p-2 flex py-20'
+      className='w-full p-4'
     >
       <div>
         <p className='text-xl tracking-widest uppercase text-[#46a8fd]'>
           Projects
         </p>
-        <div className='max-w-[1240px] m-auto flex flex-col md:grid lg:grid grid-cols-3 pt-4 gap-8'>
+        <div className='max-w-[1240px] mx-auto flex flex-col md:grid lg:grid grid-cols-3 pt-4 gap-8'>
           <div className='col-span-1'>
             <h2 className='py-4'>
               Pantry
@@ -36,42 +40,42 @@ const Projects = () => {
                 Stack:
               </h3>
               <div className='grid grid-cols-2 m-1'>
-                <div className='flex items-center m-2 w-[40px] md:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
                   <Image
                     alt='/'
                     src={Javascript}
                   />
                   <h3 className='pl-2'>JavaScript</h3>
                 </div>
-                <div className='flex items-center m-2 w-[40px] md:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
                   <Image
                     alt='/'
                     src={Node}
                   />
                   <h3 className='pl-2'>NodeJS</h3>
                 </div>
-                <div className='flex items-center m-2 w-[40px] md:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
                   <Image
                     alt='/'
                     src={Typescript}
                   />
                   <h3 className='pl-2'>Typescript</h3>
                 </div>
-                <div className='flex items-center m-2 w-[40px] md:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
                   <Image
                     alt='/'
                     src={Postgres}
                   />
                   <h3 className='pl-2'>PostgreSQL</h3>
                 </div>
-                <div className='flex items-center m-2 w-[40px] md:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
                   <Image
                     alt='/'
                     src={Firebase}
                   />
                   <h3 className='pl-2'>Firebase</h3>
                 </div>
-                <div className='flex items-center m-2 w-[40px] md:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
                   <Image
                     alt='/'
                     src={Reactlogo}
@@ -81,8 +85,8 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          <div className='col-span-2'>
-            <Pantry/>
+          <div className='col-span-2 md:pt-5 lg:pt-8'>
+            <Pantry handleLink={handleLink}/>
           </div>
         </div>
       </div>
