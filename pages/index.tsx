50a25c6fffba506c39cba905379ff8fd48169014
Ed Cuevas <edjanncuevas@icloud.com>
2023-03-16 20:20:06 +0900

    feat: change title

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Ed | Front End Developer</title>
+        <title>Ed | Developer</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href={"/favicon.ico"} />
       </Head>
