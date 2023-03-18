18404d043db199cbe0107a3663847499d620032e
Ed Cuevas <edjanncuevas@icloud.com>
2023-03-18 22:11:36 +0900

    fix: set video start at demo

--- components/BookXChange.tsx
+++ components/BookXChange.tsx
@@ -13,7 +13,7 @@ const Pantry: React.FC<BookXChangeProps> = ({ handleLink }) => {
         <div className='h-[172px] w-[315px] md:h-[250px] md:w-[450px] lg:h-[390px] lg:w-[708px] m-auto relative group'>
             <div className="aspect-w-16 aspect-h-9">
                 <iframe
-                    src="https://www.youtube.com/embed/f1bMJRe4qgs?start=26"
+                    src="https://www.youtube.com/embed/f1bMJRe4qgs?start=98"
                     title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 />
