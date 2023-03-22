29f22260bc1be630f066bee0696971e2e698dbd9
Ed Cuevas <edjanncuevas@icloud.com>
2023-03-22 12:15:59 +0900

    feat: add a gif of the mobile app

--- components/BookXChange.tsx
+++ components/BookXChange.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import Image from 'next/image';
+import React from 'react'
+import App from '../public/images/projects/app.gif'
 
 interface BookXChangeProps {
     handleLink: (arg: string) => void;
@@ -7,10 +9,15 @@ interface BookXChangeProps {
 const Pantry: React.FC<BookXChangeProps> = ({ handleLink }) => {
 
     return (
-        <div className='h-[172px] w-[315px] md:h-[250px] md:w-[450px] lg:h-[390px] lg:w-[708px] m-auto relative group'>
-            <div className="aspect-w-16 aspect-h-9">
+        <div className='flex flex-col md:flex-col h-[402px] w-[315px] md:h-[250px] md:w-[450px] lg:h-[390px] lg:w-[708px] m-auto relative group'>
+            <div className='absolute left-[30%] md:left-0 md:bottom-20 z-50'>
+                <div className='border-2 rounded-lg hover:scale-125 ease-in duration-500 bg-black'>
+                    <Image className='p-2 w-[100px]' alt='BookXChange demo' src={App}></Image>
+                </div>
+            </div>
+            <div className="mt-[210px] md:mt-0 md:ml-[70px] aspect-w-16 aspect-h-9">
                 <iframe
-                    src="https://www.youtube.com/embed/f1bMJRe4qgs?start=98"
+                    src={"https://www.youtube.com/embed/f1bMJRe4qgs?start=98&amp;rel=0&amp;wmode=transparent"}
                     title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 />
