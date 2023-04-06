2ec30ae13bef8434d8aef246e4b1518d2b0e9403
Ed Cuevas <edjanncuevas@icloud.com>
2023-04-06 16:35:13 +0900

    feat: insert pantry v2 into projects component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,6 +14,7 @@ import JavaLogo from '../public/images/skills/javalogo.png'
 import Tailwind from '../public/images/skills/tailwind.png'
 import NextJS from '../public/images/skills/nextjs.png'
 import { FiLink, FiGithub } from 'react-icons/fi'
+import Pantryv2 from './Pantryv2'
 
 interface ProjectsProps {
   handleLink: (arg: string) => void,
@@ -34,6 +35,86 @@ const Projects: React.FC<ProjectsProps> = ({ handleLink, darkMode }) => {
           </p>
         </div>
         <div className='flex flex-col justify-center h-full pt-5'>
+        <div className='project'>
+            <div className='col-span-1'>
+              <div className='flex justify-start items-center'>
+                <h2 className='py-4'>
+                  Pantry/v2
+                </h2>
+                <div className='rounded p-1 m-2 bg-[#45a7fd] text-white hover:scale-105 ease-in duration-300 cursor-pointer'>
+                  <FiLink
+                    size={20}
+                    onClick={() => handleLink('https://pantryv2.herokuapp.com/')}
+                  />
+                </div>
+                <div className='rounded p-1 m-2 bg-[#45a7fd] text-white hover:scale-105 ease-in duration-300 cursor-pointer'>
+                  <FiGithub
+                    size={20}
+                    onClick={() => handleLink('https://github.com/EdjannCuevas/pantry-2.0')}
+                  />
+                </div>
+              </div>
+              <p>Pantry is a web app that helps users manage their pantry inventory and find recipes using ingredients they already have at home. With a simple interface and easy-to-use features, Pantry makes meal planning and grocery shopping a breeze.</p>
+              <div className='w-full'>
+                <h3 className='text-xl py-5'>
+                  Stack:
+                </h3>
+                <div className='grid grid-cols-2 m-1'>
+                  <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <Image
+                      alt='/'
+                      width={35}
+                      src={Javascript}
+                    />
+                    <h3 className='pl-2'>JavaScript</h3>
+                  </div>
+                  <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <Image
+                      alt='/'
+                      width={35}
+                      src={Node}
+                    />
+                    <h3 className='pl-2'>NodeJS</h3>
+                  </div>
+                  <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <Image
+                      alt='/'
+                      width={35}
+                      src={Typescript}
+                    />
+                    <h3 className='pl-2'>Typescript</h3>
+                  </div>
+                  <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <Image
+                      alt='/'
+                      width={35}
+                      src={Postgres}
+                    />
+                    <h3 className='pl-2'>PostgreSQL</h3>
+                  </div>
+                  <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <Image
+                      alt='/'
+                      width={35}
+                      src={Tailwind}
+                    />
+                    <h3 className='pl-2'>Tailwind</h3>
+                  </div>
+                  <div className='flex items-center m-2 w-[25px] md:w-[30px] lg:w-[45px] rounded shadow-lg shadow-grey-400 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <Image
+                      alt='/'
+                      width={35}
+                      src={ReactLogo}
+                    />
+                    <h3 className='pl-2'>ReactJS</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-span-2 md:pt-5 lg:pt-8'>
+              <Pantryv2 handleLink={handleLink}/>
+            </div>
+          </div>
           <div className='project'>
             <div className='col-span-1'>
               <div className='flex justify-start items-center'>
